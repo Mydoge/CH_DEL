d860341a8464dae5753a9b7c809893c9486d8165
Mydoge <LoganLMB2008@hotmail.com>
2019-07-29 09:20:32 -0400

    Create index.ml

---
+++ index.ml
@@ -0,0 +1,11 @@
+
+<body>
+	<main>
+		<div class="intro">ChatCentral</div>
+		<div class="tagline">This website no longer exists. | Thank you for your continued support</div>
+		<div class="join">You can still join our server <a href="https://discord.gg/qWxChjD">here</a>
+	
+	</div></main>
+
+
+</body></html>
